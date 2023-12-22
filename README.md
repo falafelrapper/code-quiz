523bc9a7953ff6715419a8daf672f5bec27480eb
Zachary Roy <vgd199@gmail.com>
2023-12-21 22:53:52 -0800

    added a placeholder README, more styles, and a button that functions to start the quiz

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# code-quiz
+# Fullstack Development Challenge 4
+
+## Welcome to the fourth challenge for the UCLA edX full stack development bootcamp! 
+
+In this challenge we were supposed to come up with a coding quiz. Lets start with our user story and acceptance criteria:
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+## The Completed Challenge
+As you can see below, we now have a completed coding quiz in which you can play and attempt to get the high score on! You can see this below:
+
+![alt text](./assets/images/password-generator.png "Completed Password Generator")
+
+To visit this site and try out all the different combinations, you may click [here](https://falafelrapper.github.io/js-password-generator/)! Feel free to try different combinations of passwords to generate!
+
+## Credits
+This refactor was completed by Zachary Roy with help from Bryan Swarthout via Office Hours.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,83 @@
+/*
+RULE OF THUMB FOR THIS HW ASSIGNMENT
+1. Start button that leads to the quiz starting
+2. Quiz has a functional score and timer system
+3. Each correct answer button press leads to the next question
+4. Incorrect answer buttons decreases time from the timer
+5. Quiz ends when the timer ends or the answers are all correct
+6. When it is over, you can save your data to a leaderboard that is saved to your localStorage
+7. There is a functional leaderboard button available at the start
+*/
+
 // html el
 var startQuiz = document.querySelector("#start")
+var questionDiv = document.querySelector('#quests')
+var answersDiv = document.querySelector("#answers")
+var headerEl = document.querySelector('.main-page-header')
+var timerEl = document.querySelector('#timer')
 
 // vars
+var timer = 60;
+
+// Question arrays
+var currentQuestion = 0;
+var currentAnswer = 0;
+
+var questions = [
+    "What is a boolean?",
+    "What does CSS mean?",
+    "What does HTML mean"
+]
+
+var answers = [
+    answer1,
+    answer2,
+    answer3
+]
+
+var answer1 = [
+    "a true or false statement",
+    "a type of boot",
+    "a type of pasta",
+    "a string of code"
+]
+
+var answer2 = [
+    "Computer Science Studies",
+    "Cascading Style Sheet",
+    "Chicken Salad Sunday",
+    "Classic Salty Soup"
+]
+
+var answer3 = [
+    "a console log",
+    "A graphics processor",
+    "A type of Mercedes",
+    "HyperText Markup Language"
+]
+
+answer1[0] = true;
+answer2[1] = true;
+answer3[3] = true;
+
+function endgame(){
+    
+}
 
 function quiz(){
+    headerEl.setAttribute('class', 'hidden');
+    console.log(questionDiv);
+    questionDiv.innerHTML = "<p>" + questions[currentQuestion] + "</p>";
+    answersDiv.innerHTML = "<ul>" + answers[currentAnswer] + "</ul>";
 
+    setInterval(function(){
+        timerEl.textContent = "timer: " + timer;
+        timer--;
+    }, 1000)
+
+    if (timer === 0){
+        endGame()
+    }
 }
 
 startQuiz.addEventListener("click", quiz);
